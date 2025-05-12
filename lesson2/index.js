d7492057f635db34796230fad916cbb2012ce1fb
Konstantin <konstantin.yakovlev@grphn.ru>
2025-05-12 16:38:26 +0400

    finished lesson3

--- lesson2/index.js
+++
@@ -1,9 +0,0 @@
-const { myPackage } = require("mpackage");
-console.log(
-  "mpackage: ",
-  myPackage({
-    name: "иван иванов",
-    dateBirth: "10.11.1987",
-    purpose: "карьерный рост",
-  })
-);
