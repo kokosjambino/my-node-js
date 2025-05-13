869d43fe29c728c0a63e835055d70195768c0944
Konstantin <konstantin.yakovlev@grphn.ru>
2025-05-13 15:55:20 +0400

    improve code

--- lesson4/index.js
+++ lesson4/index.js
@@ -25,19 +25,38 @@ class EventMessage extends EventEmitter {}
 
 const eventMessage = new EventMessage();
 
+eventMessage.on('error', errMessage => {
+  console.log(errMessage);
+});
+
 function sendMessage(username, message) {
-  eventMessage.emit('processMessage', username, message);
+  if (!username || !message) {
+    eventMessage.emit(
+      'error',
+      new Error(
+        'Имя пользователя и сообщение обязательны. Сообщение не было отправлено',
+      ),
+    );
+    return;
+  }
+  eventMessage.emit('processMessage', { username, message });
 }
 
 function receiveMessage() {
-  eventMessage.addListener('processMessage', (username, message) => {
-    console.log(`Пользователь ${username} отправил сообщение ${message}`);
+  eventMessage.on('processMessage', messageData => {
+    console.log(
+      `Пользователь ${messageData.username} отправил сообщение ${messageData.message}`,
+    );
   });
 }
 
 receiveMessage();
 
 sendMessage('Алиса', 'Привет всем!');
+// Для проверки, что код не падает при ошибке
+sendMessage('', 'Привет всем!');
+sendMessage('Алиса', '');
+//
 sendMessage('Борис', 'Привет , рад тебя видеть ');
 sendMessage('Денис', 'У нас всё работает?');
 sendMessage('Алиса', 'Да , на 5 баллов!');
